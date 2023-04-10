02ef6f0787da34cc898c9676988ff04ed63d4f9e
Mychael Castro <mychael.castro@kfc.com.ec>
2023-04-10 14:32:20 -0500

    prueba1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -137,7 +137,7 @@
             </div>
         </div>
     </body>
-</html> --}}
+</html>
 
 
-<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Web site created using create-react-app"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css"/><link rel="stylesheet" href="css/bootstrap.css"/><link href="https://fonts.googleapis.com/css?family=Dosis:400,500|Poppins:400,700&amp;display=swap" rel="stylesheet"/><link href="css/style.css" rel="stylesheet"/><link href="css/responsive.css" rel="stylesheet"/><script type="text/javascript" src="js/jquery-3.4.1.min.js"></script><script type="text/javascript" src="js/bootstrap.js"></script><title>React App</title><script defer="defer" src="/static/js/main.d939ccb0.js"></script><link href="/static/css/main.31d6cfe0.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>
+{{-- <!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Web site created using create-react-app"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css"/><link rel="stylesheet" href="css/bootstrap.css"/><link href="https://fonts.googleapis.com/css?family=Dosis:400,500|Poppins:400,700&amp;display=swap" rel="stylesheet"/><link href="css/style.css" rel="stylesheet"/><link href="css/responsive.css" rel="stylesheet"/><script type="text/javascript" src="js/jquery-3.4.1.min.js"></script><script type="text/javascript" src="js/bootstrap.js"></script><title>React App</title><script defer="defer" src="/static/js/main.d939ccb0.js"></script><link href="/static/css/main.31d6cfe0.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html> --}}
